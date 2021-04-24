45684353d4c2d131ee94c4ddbd575016db4fe06e
CastroOlympias <blackcarrera@msn.com>
2021-04-24 16:03:05 -0600

    <tourism api seems ago, added to tourism js file>

--- assets/js/tourism.js
+++ assets/js/tourism.js
@@ -1,2 +1,14 @@
 console.log(10+10);
 
+fetch("https://www.triposo.com/api/20210317/location.json?id=London&account=6OIHILXY&token=cd6hxbcw7b7ujjuv48p0pjiqo5bb2zp3")
+
+.then(function(data) {
+  console.log(data);
+    console.log(data.json());
+    
+})
+.catch(err => {
+	console.error(err);
+});
+
+console.log(2*2)
