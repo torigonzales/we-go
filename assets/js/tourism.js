ce4334de7329c90e303cfe3ce118dbf5760eb2a3
CastroOlympias <blackcarrera@msn.com>
2021-04-24 18:08:01 -0600

    <two tourism fetches, created, observe and commented features of each (basic) explanations>

--- assets/js/tourism.js
+++ assets/js/tourism.js
@@ -10,8 +10,8 @@ function findCity(event) {
     var citySearch = document.querySelector('#search').value
     console.log(citySearch)
 
-
-    fetch('https://www.triposo.com/api/20210317/location.json?part_of=' + citySearch + '&account=6OIHILXY&token=cd6hxbcw7b7ujjuv48p0pjiqo5bb2zp3')
+// this fetch searches by country or cities. Searching by country find attribute captical of country - for a map view with wiki articles and images of laction.  Searching by city will list suburbes and list out images
+fetch('https://www.triposo.com/api/20210317/location.json?part_of=' + citySearch + '&account=6OIHILXY&token=cd6hxbcw7b7ujjuv48p0pjiqo5bb2zp3')
 
 .then(function(data) {
   console.log(data);
@@ -24,8 +24,17 @@ function findCity(event) {
 
 
 
+// this fetch will find points of interest (food) locations by city name. Hard code search option values into radio buttons that will populate from the country fetch search
+fetch('https://www.triposo.com/api/20210317/poi.json?location_id=' + citySearch + '&tag_labels=eatingout&account=6OIHILXY&token=cd6hxbcw7b7ujjuv48p0pjiqo5bb2zp3')
 
-
+.then(function(data) {
+  console.log(data);
+    console.log(data.json());
+    
+})
+.catch(err => {
+	console.error(err);
+});
 
 };
 
@@ -42,14 +51,3 @@ function findCity(event) {
 
 
 
-
-// fetch("https://www.triposo.com/api/20210317/location.json?id=London&account=6OIHILXY&token=cd6hxbcw7b7ujjuv48p0pjiqo5bb2zp3")
-
-// .then(function(data) {
-//   console.log(data);
-//     console.log(data.json());
-    
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
