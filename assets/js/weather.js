54e0cb8c469d8f3f92c26b02df1377b2a964b514
CastroOlympias <blackcarrera@msn.com>
2021-04-26 17:54:43 -0600

    <added another div section to the html>

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,23 +1,23 @@
-// // Current weather API pull with minutely data
-// fetch("https://api.weatherbit.io/v2.0/current?&city=Salt Lake City&country=US&key=605fec257fb24bfc849577002a714395&include=minutely")
+// Current weather API pull with minutely data
+fetch("https://api.weatherbit.io/v2.0/current?&city=Amsterdam&country=US&key=605fec257fb24bfc849577002a714395&include=minutely")
 
-// .then(function(data) {
-//   console.log(data);
-//     console.log(data.json());
+.then(function(data) {
+  console.log(data);
+    console.log(data.json());
     
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
+})
+.catch(err => {
+	console.error(err);
+});
 
-// // 16 Day weather forecast API
-// fetch("https://api.weatherbit.io/v2.0/forecast/daily?&city=Salt Lake City&country=US&key=605fec257fb24bfc849577002a714395&include=minutely")
+// 16 Day weather forecast API
+fetch("https://api.weatherbit.io/v2.0/forecast/daily?&city=Salt Lake City&country=US&key=605fec257fb24bfc849577002a714395&include=minutely")
 
-// .then(function(data) {
-//   console.log(data);
-//     console.log(data.json());
+.then(function(data) {
+  console.log(data);
+    console.log(data.json());
     
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
+})
+.catch(err => {
+	console.error(err);
+});
